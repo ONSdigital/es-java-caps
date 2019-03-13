79da81797a71625d15d857e4952a15958043806a
ons <adrian.harris@ons.gov.uk>
2019-03-13 23:44:41 +0000

    Algorithmia initial commit

---
+++ src/main/java/algorithmia/Goodbyecruelworld/Goodbyecruelworld.java
@@ -0,0 +1,13 @@
+package algorithmia.Goodbyecruelworld;
+
+import com.algorithmia.*;
+import com.algorithmia.algo.*;
+import com.algorithmia.data.*;
+import com.google.gson.*;
+import java.util.*;
+
+public class Goodbyecruelworld {
+    public String apply(String s) throws Exception {
+        return "Hello " + s;
+    }
+}
